d36e8d76ade4dbb677e68c46e98dd1cb5e79535c
abhi1-2 <abhishek.sarkar698@gmail.com>
2019-01-26 14:24:40 +0530

    server and dynamic page added

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask ,render_template
 from predict_by_image import gender_predictor
 import os
 current_dir=os.getcwd()
-app=Flask(__name__)
-app.config['UPLOAD_FOLDER'] = os.path.join('web_app', 'test_images')
+app=Flask(__name__,static_url_path = "/test_images", static_folder = "test_images")
+
 
 
 @app.route('/home')
@@ -12,7 +12,7 @@ def home():
 	print(current_dir)
 	img_path=current_dir+'/test_images/srija_test.jpeg'
 	gender=gender_predictor(img_path)
-	full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'srija_test.jpeg')
+	full_filename='/test_images/srija_test.jpeg'
 	return render_template('home.html',gender=gender,image=full_filename)
 
 
